719a2e751aaa89a89fbd9c8adf9aebb619d16a53
linh.nv <nguyenvanlinh1410@gmail.com>
2026-01-14 17:27:19 +0900

    updated

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "../utils/fixtures";
 import { LoginPage } from "../pages/LoginPage";
-
+const userName = process.env.STANDARD_USERNAME || "standard_user";
+const password = process.env.STANDARD_PASSWORD || "secret_sauce";
 test.describe("Login Functionality", () => {
   test.beforeEach(async ({ loginPage }) => {
     await loginPage.navigate();
@@ -10,14 +11,14 @@ test.describe("Login Functionality", () => {
     page,
     loginPage,
   }) => {
-    await loginPage.login("standard_user", "secret_sauce");
+    await loginPage.login(userName, password);
 
     // Initial basic assertion just to verify we are logged in (URL change or element presence)
     await expect(page).toHaveURL(/inventory\.html/);
   });
 
   test("should show error with invalid credentials", async ({ loginPage }) => {
-    await loginPage.login("locked_out_user", "secret_sauce");
+    await loginPage.login("locked_out_user", password);
     await loginPage.verifyErrorMessage("Sorry, this user has been locked out.");
   });
 });
