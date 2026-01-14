3ba63680f3d23ec3ae8146cf1f080142d1a83a55
linh.nv <nguyenvanlinh1410@gmail.com>
2026-01-14 18:35:29 +0900

    updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
+
   use: {
     baseURL: "https://www.saucedemo.com",
     trace: "on-first-retry",
@@ -19,7 +20,12 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          slowMo: 0,
+        },
+      },
     },
     {
       name: "firefox",
